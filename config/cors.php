3fc1ee0374412efe0c37ff56616c1f386c7e6786
teguhpermadi <tguh.permadi@gmail.com>
2026-02-27 04:09:38 +0700

    feat: Add initial CORS configuration file.

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'http://192.168.1.2:5173'],
 
     'allowed_origins_patterns' => [],
 
